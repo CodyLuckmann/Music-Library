1da3b391d3a459fffa058e956c14d1a5cbdd91f2
CodyLuckmann <cluckmann@live.com>
2022-01-11 15:58:06 -0600

    created NavBar, not functionable though

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+
+const NavBar = (props) => {
+    return ( 
+        <div class='topnav'>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light"></nav>
+        </div>
+     );
+}
+ 
+export default NavBar;
