bdbbc54143425da5cbab9eb0076c5d34c3c9b1d1
CodyLuckmann <cluckmann@live.com>
2022-01-11 15:45:08 -0600

    function for filtering started but not working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 import DisplayMusic from './Components/DisplayMusic/DisplayMusic';
+import SearchBar from './Components/SearchBar/SearchBar';
 
 function App() {
 
@@ -10,14 +11,28 @@ function App() {
   useEffect(() => {
     getAllSongs()
   }, []);
+  //TODO: Write a filterSongs function and pass it down to the SearchBar component
+  // HINT: look at Most Wanted filtering!
+  // use searchTerm (from the SearchBar) to filter songs
+  function filterSongs(song){
+    let foundSongs = songs.filter(function(searchTerm){
+      if(searchTerm ==''){
+        return foundSongs
+      }
+      else if (song.title.toLowerCase().includes(searchTerm.toLowerCase())){
+        return foundSongs
+      }
+    })
+  }
 
   async function getAllSongs(){
     let response = await axios.get('http://www.devcodecampmusiclibrary.com/api/music');
     setSongs(response.data);
   } 
   return(
-    <div>
-      <div className='border-box'>
+    <div className='background'>
+      <div className='border-box' className='table-font'>
+        <SearchBar filterSongs={filterSongs}/>
         <DisplayMusic songs={songs}/>
       </div>
       
