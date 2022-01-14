e718002d6b767bc2bfd23c3d35282d6cb81cadbf
CodyLuckmann <cluckmann@live.com>
2022-01-14 10:32:38 -0600

    changed axios call to access my backend

--- src/App.js
+++ src/App.js
@@ -28,7 +28,8 @@ function App() {
   }
 
   async function getAllSongs(){
-    let response = await axios.get('http://www.devcodecampmusiclibrary.com/api/music');
+    let response = await axios.get('http://127.0.0.1:8000/music/');
+    console.log(response)
     setSongs(response.data);
   } 
   return(
