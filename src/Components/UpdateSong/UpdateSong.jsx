b6882438c2fb6266816f37ff4ec6ed3f25387d18
CodyLuckmann <cluckmann@live.com>
2022-01-17 14:40:36 -0600

    fixed update song form

--- src/Components/UpdateSong/UpdateSong.jsx
+++ src/Components/UpdateSong/UpdateSong.jsx
@@ -1,41 +1,43 @@
 import React, {useState} from "react";
 
-const UpdateForm = (props) => {
+const UpdateSong = (props) => {
 
-    const [title, setTitle] = useState('');
-    const [album, setAlbum] = useState('');
-    const [artist, setArtist] = useState('');
-    const [genre, setGenre] = useState('');
-    const [releaseDate, setReleaseDate] = useState('');
+    const [title, setTitle] = useState(props.song.title);
+    const [album, setAlbum] = useState(props.song.album);
+    const [artist, setArtist] = useState(props.song.artist);
+    const [genre, setGenre] = useState(props.song.genre);
+    const [releaseDate, setReleaseDate] = useState(props.song.release_date);
 
     function handleSubmit(formEvent){
         formEvent.preventDefault();
-        let newSong = {
+        let updatedSong = {
             title: title,
             album: album,
             artist: artist,
             genre: genre,
             release_date: releaseDate,
-
         }
-        props.updateSong(newSong);
+        props.updateSong(props.song.id ,updatedSong);
     }
 
     return (
+        <>
+       
         <form onSubmit={handleSubmit}>
             <label>Title</label>
-            <input type='text' onChange={(event) => setTitle(event.target.value)} value={title}/>
+            <input type='text' onChange={(event) => setTitle(event.target.value)} defaultValue={title}/>
             <label>Album</label>
-            <input type='text' onChange={(event) => setAlbum(event.target.value)} value={album}/>
+            <input type='text' onChange={(event) => setAlbum(event.target.value)} defaultValue={album}/>
             <label>Artist</label>
-            <input type='text' onChange={(event) => setArtist(event.target.value)} value={artist}/>
+            <input type='text' onChange={(event) => setArtist(event.target.value)} defaultValue={artist}/>
             <label>Genre</label>
-            <input type='text' onChange={(event) => setGenre(event.target.value)} value={genre}/>
+            <input type='text' onChange={(event) => setGenre(event.target.value)} defaultValue={genre}/>
             <label>Release Date</label>
-            <input type='date' onChange={(event) => setReleaseDate(event.target.value)} value={releaseDate}/>
+            <input type='date' onChange={(event) => setReleaseDate(event.target.value)} defaultValue={releaseDate}/>
             <button type='submit'>Edit</button>
         </form>
+        </>
     );
 }
 
-export default UpdateForm;
+export default UpdateSong;
